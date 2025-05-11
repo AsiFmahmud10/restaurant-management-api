37b4f7af4611a7f278577c58082229e55796edb6
AsifMahmud10 <mhamudasif2@gmail.com>
2025-05-11 18:24:01 +0600

    add swagger & developer exception page

--- RestaurantManagement/Program.cs
+++ RestaurantManagement/Program.cs
@@ -1,4 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "My API V1");
+    } );
+}
+
 
 app.Run();
