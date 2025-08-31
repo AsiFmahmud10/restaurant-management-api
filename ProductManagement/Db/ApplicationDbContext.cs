2bbc1474ba4757b9be84b052086c9b7db44f4ccb
AsifMahmud10 <mhamudasif2@gmail.com>
2025-09-01 04:10:43 +0600

    configure many-to-many relationship between roles and permissions

--- ProductManagement/Db/ApplicationDbContext.cs
+++ ProductManagement/Db/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 using ProductManagement.Product;
 
 namespace ProductManagement.Db;
+
 using Microsoft.EntityFrameworkCore;
 using Cart;
 using User;
@@ -8,20 +9,24 @@ using Role;
 using Order;
 using Product;
 using Token;
+using Permission;
+
 public class ApplicationDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
     public DbSet<Role> Roles { get; set; }
-    public DbSet<Product> Products { get; set; } 
+    public DbSet<Permission> Permission { get; set; }
+    public DbSet<Product> Products { get; set; }
     public DbSet<Cart> Carts { get; set; }
     public DbSet<CartItem> CartItems { get; set; }
-    public DbSet<Order> Orders {get;set;}
-    public DbSet<OrderItem> OrderItems {get;set;}
-    public DbSet<Category> Categories {get;set;}
-    public DbSet<Token> Token {get;set;}
-    
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
-    { }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Token> Token { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -29,43 +34,47 @@ public class ApplicationDbContext : DbContext
             .HasOne(o => o.User)
             .WithOne(user => user.Order)
             .HasForeignKey<Order>(o => o.UserId);
-        
+
         modelBuilder.Entity<Order>()
             .HasMany(o => o.OrderItems)
             .WithOne()
             .HasForeignKey(o => o.OrderId)
             .OnDelete(DeleteBehavior.Cascade);
-        
+
         modelBuilder.Entity<OrderItem>()
             .HasOne(o => o.Product)
             .WithMany()
-            .HasForeignKey(o=> o.ProductId);
-        
+            .HasForeignKey(o => o.ProductId);
+
         modelBuilder.Entity<Cart>()
             .HasMany(c => c.CartItems)
             .WithOne()
             .HasForeignKey(c => c.CartId)
             .OnDelete(DeleteBehavior.Cascade);
-        
+
         modelBuilder.Entity<CartItem>()
             .HasOne(c => c.Product)
             .WithMany()
-            .HasForeignKey(c=> c.ProductId);
-        
+            .HasForeignKey(c => c.ProductId);
+
         modelBuilder.Entity<Category>()
-            .HasMany(c=> c.Products)
+            .HasMany(c => c.Products)
             .WithOne(p => p.Category)
             .HasForeignKey(p => p.CategoryId);
 
         modelBuilder.Entity<User>()
             .HasMany(u => u.Roles)
             .WithMany();
-        
+
         modelBuilder.Entity<User>()
             .HasMany(u => u.Tokens)
-            .WithOne(token => token.User )
+            .WithOne(token => token.User)
             .HasForeignKey(t => t.UserId);
-        
+
+        modelBuilder.Entity<Role>()
+            .HasMany(r => r.Permissions)
+            .WithMany();
+
         base.OnModelCreating(modelBuilder);
     }
 
@@ -86,6 +95,4 @@ public class ApplicationDbContext : DbContext
         }
         return base.SaveChanges();
     }
-
-    
 }
