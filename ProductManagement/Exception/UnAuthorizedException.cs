a97a5b83ffd7beda69076b01e88d3fa687a1f850
AsifMahmud10 <mhamudasif2@gmail.com>
2025-10-22 00:11:47 +0600

    change method visibility

--- ProductManagement/Exception/UnAuthorizedException.cs
+++ ProductManagement/Exception/UnAuthorizedException.cs
@@ -2,6 +2,5 @@ namespace ProductManagement.Exception;
 
 public class UnAuthorizedException(string message) : System.Exception
 {
-    private string Message { get; set; } = message;
-
+    public string Message { get; set; } = message;
 }
