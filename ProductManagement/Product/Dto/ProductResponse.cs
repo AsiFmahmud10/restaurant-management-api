f68a6e9a849e6e1de93e7f7306bf325d303e36be
asif <mhamudasif2@gmail.com>
2025-12-06 21:26:31 +0600

    feat: add Product id to ProductResponse

--- ProductManagement/Product/Dto/ProductResponse.cs
+++ ProductManagement/Product/Dto/ProductResponse.cs
@@ -2,6 +2,7 @@ namespace ProductManagement.Product.Dto;
 
 public class ProductResponse
 {
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
